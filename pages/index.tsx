60eda6347a1991039a6eddef98c3a0fb1a879cdf
Nicklas Kenyon <nick@kinoteksoftware.com>
2022-11-14 11:24:18 -0500

    add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,10 @@ export default function Home() {
       <Head>
         <title>Radiant Gardeners</title>
         <meta name="description" content="A couple creating cool stuff on the internet together" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <header className={styles.header}>
