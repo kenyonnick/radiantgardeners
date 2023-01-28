2a8b5d9f6561370d1a35a752ea1088300f89cbdf
Nicklas Kenyon <nick@kinoteksoftware.com>
2023-01-28 18:07:15 -0500

    final scoreboard

--- src/components/partyfest/Scoreboard/Scoreboard.tsx
+++ src/components/partyfest/Scoreboard/Scoreboard.tsx
@@ -1,52 +1,103 @@
 import { Team, YELLOW, PURPLE, GREEN, ORANGE } from '../../../data';
 import styles from './Scoreboard.module.css';
 
-type Day = {
+type ScoreRow = {
+    label: string;
     green?: number;
     orange?: number;
     purple?: number;
     yellow?: number;
     winner?: string;
-    winningTeam?: Team;
+    winningColor?: string;
+    winningGradient?: string,
 };
 
-const scores: Day[] = [
+const scores: ScoreRow[] = [
     {
+        label: 'Day 1',
         green: 12,
         orange: 6,
         purple: 12,
         yellow: 15,
         winner: 'Jeice',
-        winningTeam: YELLOW,
+        winningColor: YELLOW.color,
     },
     {
+        label: 'Day 2',
         green: 13,
         orange: 10,
         purple: 14,
         yellow: 9,
         winner: 'Lilindo_s', 
-        winningTeam: PURPLE,
+        winningColor: PURPLE.color,
     },
     {
+        label: 'Day 3',
         green: 11,
         orange: 12,
         purple: 13,
         yellow: 7,
         winner: 'xIlyria',
-        winningTeam: PURPLE,
+        winningColor: PURPLE.color,
     },
     {
+        label: 'Day 4',
         green: 9,
         orange: 14,
         purple: 9,
         yellow: 10,
         winner: 'NotoriousCRANK',
-        winningTeam: ORANGE,
+        winningColor: ORANGE.color,
     },
-    {},
+    {
+        label: 'Day 5',
+        green: 12,
+        orange: 24,
+        purple: 26,
+        yellow: 24,
+        winner: 'Purple',
+        winningColor: PURPLE.color,
+    },
+    {
+        label: 'Team Spirit',
+        green: 2,
+        winner: 'Green',
+        winningColor: GREEN.color,
+    },
+    {
+        label: 'Craziest Duel',
+        green: 1,
+        purple: 1,
+        winner: 'KTWayfinder & Lilindo',
+        winningGradient: `linear-gradient(to right, ${GREEN.color}, ${PURPLE.color}`,
+    },
+    {
+        label: 'Best Trash Talk',
+        yellow: 2,
+        winner: 'AriefJohan',
+        winningColor: YELLOW.color,
+    },
+    {
+        label: 'Absolutely Robbed',
+        yellow: 2,
+        winner: 'LanzOfMasters',
+        winningColor: YELLOW.color,
+    },
+    {
+        label: 'Greatest Showman',
+        orange: 2,
+        winner: 'Mochocalate9k',
+        winningColor: ORANGE.color,
+    },
+    {
+        label: 'Chat Pick',
+        green: 5,
+        winner: 'Green',
+        winningColor: GREEN.color,
+    }
 ];
 
-const teamScore = (days: Day[], team: keyof Omit<Day, 'winner' | 'winningTeam'>) => {
+const teamScore = (days: ScoreRow[], team: keyof Omit<ScoreRow, 'label' | 'winner' | 'winningColor' | 'winningGradient'>) => {
     return days.map((day) => day[team]).reduce((prev, curr) => {
         if(curr) return (prev ?? 0) + curr;
         else return prev;
@@ -58,7 +109,7 @@ export const Scoreboard = () => {
         <table className={styles.table}>
             <thead>
                 <tr>
-                    <td>Day</td>
+                    <td></td>
                     <td align='right'>Green</td>
                     <td align='right'>Orange</td>
                     <td align='right'>Purple</td>
@@ -70,13 +121,13 @@ export const Scoreboard = () => {
                 {scores.map((day, index) => {
                     return (
                         <tr key={index}>
-                            <td>{index+1}</td>
-                            <td align="right">{day.green ?? '?'}</td>
-                            <td align="right">{day.orange ?? '?'}</td>
-                            <td align="right">{day.purple ?? '?'}</td>
-                            <td align="right">{day.yellow ?? '?'}</td>
-                            <td align="center" style={{ backgroundColor: day.winningTeam?.color }}>
-                                {day.winner ?? '?'}
+                            <td>{day.label}</td>
+                            <td align="right">{day.green}</td>
+                            <td align="right">{day.orange}</td>
+                            <td align="right">{day.purple}</td>
+                            <td align="right">{day.yellow}</td>
+                            <td align="center" style={{ backgroundColor: day.winningColor, backgroundImage: day.winningGradient }}>
+                                {day.winner}
                             </td>
                         </tr>
                     )
@@ -87,7 +138,7 @@ export const Scoreboard = () => {
                     <td align="right">{teamScore(scores, 'orange')}</td>
                     <td align="right">{teamScore(scores, 'purple')}</td>
                     <td align="right">{teamScore(scores, 'yellow')}</td>
-                    <td align="center">?</td>
+                    <td align="center" style={{ backgroundColor: PURPLE.color }}>Purple</td>
                 </tr>
             </tbody>
         </table>
